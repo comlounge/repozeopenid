ba8db43d3304bf9afcc60462333c4939605f80a7
tao.takashi <tao.takashi@eab628c8-4855-0410-9f7a-8155163ddc21>
2008-11-02 23:52:55 +0000

    added test suite

--- repoze.whoplugins.openid/trunk/setup.py
+++ repoze.whoplugins.openid/trunk/setup.py
@@ -11,7 +11,14 @@ setup(name='repoze.who.plugins.openid',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP" 
         ],
       keywords='openid repoze who identification authentication plugin',
       author='Christian Scholz',
@@ -23,10 +30,14 @@ setup(name='repoze.who.plugins.openid',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'repoze.who>=1.0.6', 
+	  'python-openid>=2.0',
           'setuptools',
-          # -*- Extra requirements: -*-
-	  'python-openid>=2.0'
+          'zope.interface'
       ],
+      test_requires=[   
+      ],
+      test_suite="repoze.who.plugins.openid",
       entry_points="""
       # -*- Entry points: -*-
       """,
